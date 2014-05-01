bc03de760c85c689e038f578fb40d84b81dd3f75
John Morrison <jrgmorrison@gmail.com>
2014-04-30 21:17:56 -0700

    tbld

--- tests/intern_sauce.js
+++ tests/intern_sauce.js
@@ -10,22 +10,10 @@ define([
   // override the main config file and adjust it to suit Sauce Labs
   intern.useSauceConnect = true;
   intern.webdriver.port = 4445;
+  intern.capabilities: {},
 
   intern.environments = [
-    // XXX these two browsers currently aren't working on SauceLabs
-    //{ browserName: 'iphone', version: '6.0', platform: 'OS X 10.8' },
-    //{ browserName: 'android', version: '4.0', platform: 'Linux' },
-
-    // TODO (Issue #562) No IE support yet
-    //{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-    //{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-    //{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-    //{ browserName: 'internet explorer', version: '8', platform: 'Windows XP' },
-
-    { browserName: 'firefox', version: '28', platform: [ 'OS X 10.6'/*, 'Windows 7' */] }
-//    { browserName: 'firefox', version: '24', platform: 'Linux' },
-//   { browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
-//    { browserName: 'safari', version: '6', platform: 'OS X 10.8' }
+    { browserName: 'firefox', platform: [ 'Windows 7' ] }
   ];
 
   return intern;
