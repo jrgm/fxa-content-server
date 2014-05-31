3a62a149b6880fe8f99c1e7ac446dc3c61aea2c3
John Morrison <jrgmorrison@gmail.com>
2014-05-31 03:43:50 +0000

    pick a subset of tests to create, verify, change password, delete account

---
+++ tests/intern_smoke_subset.js
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+define([
+  './intern'
+], function (intern) {
+  'use strict';
+
+  // override the main config file and adjust it to suit Sauce Labs
+  intern.useSauceConnect = true;
+  intern.webdriver.port = 4445;
+
+  intern.functionalSuites = [ 
+    'tests/functional/sign_in',
+    'tests/functional/change_password',
+    'tests/functional/settings'
+  ];
+
+  intern.capabilities = {};
+
+  intern.environments = [
+    { browserName: 'firefox', version: '29', platform: 'Windows 7' }
+    //, { browserName: 'chrome', platform: 'Windows 7' }
+  ];
+
+  return intern;
+});
